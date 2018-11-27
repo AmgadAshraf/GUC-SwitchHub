8905d4e812dedee0a70fcc83b8e2d8b47c4235b1
Amgad Ashraf Ramses <amgadramses@yahoo.com>
2018-11-28 01:53:37 +0200

    updated database schema

--- database.sql
+++ database.sql
@@ -4,22 +4,20 @@ CREATE TABLE signedup (
     id VARCHAR(10) UNIQUE NOT NULL,
     email VARCHAR(50) PRIMARY KEY ,
     userpassword VARCHAR(50) NOT NULL
-
 );
 
 CREATE TABLE switching (
     id VARCHAR(10) UNIQUE NOT NULL,
-
     major VARCHAR(10)  NOT NULL,
     tutorialfrom VARCHAR(5) NOT NULL,
     tutorialto VARCHAR(5) NOT NULL,
-    germanlevel INTEGER,
-    englishlevel VARCHAR(10),
-    
+    germanlevel INTEGER NOT NULL,
+    englishlevel VARCHAR(10) NOT NULL,
     email VARCHAR(50) PRIMARY KEY,
-    remainingswitches int DEFAULT 2
+    remainingswitches int DEFAULT 2,
+    didswitch BOOLEAN DEFAULT false NOT NULL
 );
-
+/*
 INSERT INTO signedup VALUES
 ('Amgad','Ashraf','37-2058','amgadramses96@gmail.com','password123'),
 ('Akram','Ashraf','37-2076','akramashraf96@gmail.com','password12345');
@@ -27,5 +25,5 @@ INSERT INTO signedup VALUES
 
 /*DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
-*/
 
+*/
